1cdf7f4565c37a1184d25cfcf7f111a57e29b50e
hyacinth <antariksh0508@gmail.com>
2026-05-07 18:06:18 +0530

    Refactor digit counting to use loop method
    Updated the digit counting logic to use a loop instead of log function for reliability with negative numbers.

--- digits_of_number.c
+++ digits_of_number.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-#include <math.h>
+#include <math.h>  // we  need to add maths library to use log function , if using loop method , no need to add it .
 
 int main() {
-    int n,digit;
-printf(" enter your number :\n");
+    int n,digit=0;
+printf(" enter your number :\n"); 
 scanf("%d",&n);
- digit = log10(n)+1;
+ // digit = log10(n)+1;  // log is useful but super realiable , cause of neagative numbers and floating round offs .
+    while (n>0){
+        n = n/10;
+        digit++;
+    }
 printf(" number of digits is %d\n",digit); 
     return 0;
 }
